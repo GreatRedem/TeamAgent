07d7feb04ceb4162942c28c0c939b032b032d266
Alex <alex@example.com>
2026-09-19 07:13:11 +0330

    refactor: remove blog, server and shop modules

--- backend/src/routes/shop/shop.route.ts
+++
@@ -1,9 +0,0 @@
-import type { FastifyInstance } from 'fastify';
-
-import { list, purchase } from './shop.service.js';
-
-export default async function(fastify: FastifyInstance)
-{
-    fastify.get('/shop', list(fastify));
-    fastify.post('/shop/purchase', purchase(fastify));
-}
