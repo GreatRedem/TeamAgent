8bc909978cea2305f1b2d070e9913b7aa47e1e38
Alex <alex@example.com>
2026-09-19 07:13:16 +0330

    chore: add session secrets to env contract

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -113,67 +113,19 @@ const NODE_ENV = (() =>
     return value as 'development' | 'production';
 })();
 
-const NODE_RECOVERY_TIME = builder('NODE_RECOVERY_TIME').asDuration();
+const NODE_DB = builder('NODE_DB').asString();
 const NODE_COOKIE = builder('NODE_COOKIE').asString();
 
-const MAIL_SERVICE = builder('MAIL_SERVICE').asString();
-const MAIL_HOST = builder('MAIL_HOST').asString();
-const MAIL_PORT = builder('MAIL_PORT').asNumber();
-const MAIL_USERNAME = builder('MAIL_USERNAME').asString();
-const MAIL_PASSWORD = builder('MAIL_PASSWORD').asString();
-const MAIL_FROM = builder('MAIL_FROM').asString();
-
-const DB_WEB_TYPE = builder('DB_WEB_TYPE').asString();
-const DB_WEB_HOST = builder('DB_WEB_HOST').asString();
-const DB_WEB_PORT = builder('DB_WEB_PORT').asNumber();
-const DB_WEB_USERNAME = builder('DB_WEB_USERNAME').asString();
-const DB_WEB_PASSWORD = builder('DB_WEB_PASSWORD').asString();
-const DB_WEB_DATABASE = builder('DB_WEB_DATABASE').asString();
-const DB_WEB_SYNC = builder('DB_WEB_SYNC').asBoolean();
-const DB_WEB_LOG = builder('DB_WEB_LOG').asBoolean();
-
-const SOAP_HOST = builder('SOAP_HOST').asStringArray();
-const SOAP_PORT = builder('SOAP_PORT').asNumberArray();
-const SOAP_USERNAME = builder('SOAP_USERNAME').asStringArray();
-const SOAP_PASSWORD = builder('SOAP_PASSWORD').asStringArray();
-
-const SESSION_ACCESS_TIME = builder('SESSION_ACCESS_TIME').asDuration();
 const SESSION_ACCESS_SECRET = builder('SESSION_ACCESS_SECRET').asString();
-
-const SESSION_REFRESH_TIME = builder('SESSION_REFRESH_TIME').asDuration();
 const SESSION_REFRESH_SECRET = builder('SESSION_REFRESH_SECRET').asString();
 
 export default
 {
     NODE_PORT,
     NODE_ENV,
-    NODE_RECOVERY_TIME,
+    NODE_DB,
     NODE_COOKIE,
 
-    MAIL_SERVICE,
-    MAIL_HOST,
-    MAIL_PORT,
-    MAIL_USERNAME,
-    MAIL_PASSWORD,
-    MAIL_FROM,
-
-    DB_WEB_TYPE,
-    DB_WEB_HOST,
-    DB_WEB_PORT,
-    DB_WEB_USERNAME,
-    DB_WEB_PASSWORD,
-    DB_WEB_DATABASE,
-    DB_WEB_SYNC,
-    DB_WEB_LOG,
-
-    SOAP_HOST,
-    SOAP_PORT,
-    SOAP_USERNAME,
-    SOAP_PASSWORD,
-
-    SESSION_ACCESS_TIME,
     SESSION_ACCESS_SECRET,
-
-    SESSION_REFRESH_TIME,
     SESSION_REFRESH_SECRET
 };
